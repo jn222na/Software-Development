2124e54c63f117180c55d9a204b8a4c07a25a68a
jn222na <jn222na@student.lnu.se>
2015-11-09 11:41:57 +0100

    TEST 002 RED
    TestComboBox

--- 04-dt007g-joni510/src/Tests/TestSwingApp.java
+++ 04-dt007g-joni510/src/Tests/TestSwingApp.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.*;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.awt.event.WindowEvent;
+import java.io.Console;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import org.junit.*;
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.rules.ExpectedException;
 
 import View.MainView;
 
@@ -19,29 +25,153 @@ public class TestSwingApp {
 		MainView mv = new MainView();
 		JFrame frame = mv.drawWindow();
 		assertTrue(frame.isMinimumSizeSet());
+		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 	}
-	@Test
-	public void testSystemButtons(){
-		//Minimize
-		//Fullscreen / !Fullscreen
-		//Close
-	}
-	@Test
-	public void testTitleBar(){
-		//Move
-		//Minimize
-		//Close
-	}
+//	@Test
+//	public void testFrameResizeXYZ() {
+//		System.out.println("Initializing test, dont move your mouse");
+//		System.out.println("Testing resize");
+//		
+//		try {
+//			int mousePos =0;
+//			MainView mv = new MainView();
+//			JFrame frame = mv.drawWindow();
+//			mv.drawWindow();
+//
+//			Robot bot = new Robot();
+//			for (int i = 0; i < 5; i++) {
+//				System.out.print(".");
+//				bot.delay(1000);
+//			}
+//			for (int i = 0; i <= 100; i+=10) {
+//				bot.mouseMove((int) (frame.getLocation().getX()-i),(int)frame.getLocation().getY()+i);
+//				bot.mousePress(InputEvent.BUTTON1_MASK);
+//				bot.delay(50);
+//				mousePos = (int)frame.getLocation().getX()-i;
+//			}
+//			bot.mouseRelease(InputEvent.BUTTON1_MASK);
+//			
+//			
+//			assertEquals(50, mousePos, 0);
+//			
+//			for (int i = 0; i <= 50; i+=10) {
+//				bot.mouseMove((int)frame.getLocation().getX() , (int)frame.getLocation().getY()-i);
+//				bot.mousePress(InputEvent.BUTTON1_MASK);
+//				bot.delay(50);
+//				mousePos = (int)frame.getLocation().getY()-i;
+//			}
+//			assertEquals(50, mousePos, 0);
+//			bot.mouseRelease(InputEvent.BUTTON1_MASK);
+//			for (int i = 0; i <= 100; i+=10) {
+//				bot.mouseMove((int)frame.getLocation().getX()+i , (int)frame.getLocation().getY());
+//				bot.mousePress(InputEvent.BUTTON1_MASK);
+//				bot.delay(50);
+//				mousePos = (int)frame.getLocation().getX()+i;
+//			}
+//			assertEquals(800, mousePos, 0);
+//			bot.mouseRelease(InputEvent.BUTTON1_MASK);
+//			
+//			
+//			bot.mouseRelease(InputEvent.BUTTON1_MASK);
+//			for (int i = 0; i <= 50; i+=10) {
+//				bot.mouseMove((int)frame.getLocation().getX()-i , (int)frame.getLocation().getY()-i);
+//				bot.mousePress(InputEvent.BUTTON1_MASK);
+//				bot.delay(50);
+//				mousePos = (int)frame.getLocation().getY()+i;
+//			}
+//			assertEquals(50, mousePos, 0);
+//			bot.mouseRelease(InputEvent.BUTTON1_MASK);
+//			frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+//		} catch (AWTException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	try {
+//		//Testing exit button position
+//		MainView mv = new MainView();
+//		mv.drawWindow();
+//		
+//		Robot bot = new Robot();
+//		bot.mouseMove(1260, 270);
+//		bot.delay(1000);
+//		bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//		bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+//		
+//	} catch (AWTException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//	}
+
+	
 	@Test
 	public void testIfFrameOpens() {
 		
 			MainView mv = new MainView();
 			mv.drawWindow();
-			mv.close();
+//			mv.close();
+	}
+	
+//	@Test
+//	public void testSystemButtons(){
+//		//Minimise
+//		//Fullscreen / !Fullscreen
+//		//Exit
+//	}
+//	@Test
+//	public void testTitleBar(){
+//		//Move
+//		//Minimise
+//		//Close
+//	}
+	@Test
+	public void testComboBox(){
+		MainView mv = new MainView();
+		mv.drawWindow();
+		JComboBox<String> box = mv.jcombo();
+		try {
+			//Freehand tool
+			
+			Robot bot = new Robot();
+			bot.mouseMove(1260, 350);
+			
+			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+			bot.delay(1000);
+			bot.mouseMove(1260, 400);
+			bot.delay(1000);
+			bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+			bot.delay(1000);
+			assertTrue(box.getSelectedItem() == "Frihand");
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			//Rectangular tool
+			Robot bot = new Robot();
+			bot.mouseMove(1260, 350);
+			
+			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+			bot.delay(1000);
+			bot.mouseMove(1260, 390);
+			bot.delay(1000);
+			bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+			bot.delay(1000);
+			assertTrue(box.getSelectedItem() == "Rektangel");
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
-
-
+//	@Test
+//	public void testColorChoice(){
+//		//Colorchoice update
+//	}
+	
 
 
 }
--- 04-dt007g-joni510/src/View/MainView.java
+++ 04-dt007g-joni510/src/View/MainView.java
@@ -1,27 +1,78 @@
 package View;
 
-import java.awt.Dimension;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-
+import java.awt.*;
 import javax.swing.*;
 
 public class MainView {
-	
-	public MainView() {
-		
-	}
-	public JFrame drawWindow(){
-		JFrame frame = new JFrame("Ritprogram");
-	    frame.setVisible(true);
-	    frame.setSize(500, 500);
-	    frame.setMinimumSize(new Dimension(500, 500));
 
-	    return frame;
+	public MainView() {
+
 	}
+
+	JFrame frame = new JFrame("Ritprogram");
+	JComboBox<String> box = new JComboBox<String>();
 	
-	public void close() {
-		// TODO Auto-generated method stub
+	public JFrame drawWindow() {
+
+		frame.setVisible(true);
+		frame.setMinimumSize(new Dimension(600, 500));
+		frame.setBounds(700, 250, 600, 500);
+		frame.setLayout(new BorderLayout());
+
+		JPanel mainPanel = buildTopGrid();
+		frame.add(mainPanel, BorderLayout.PAGE_START);
+		return frame;
+	}
+	public JComboBox<String> jcombo(){
+		return this.box;
+	}
+
+	private JPanel buildTopGrid() {
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new GridLayout(1, 6));
+		mainPanel.setSize(new Dimension(frame.getWidth(), 0));
+		mainPanel.setVisible(true);
+
+		JPanel jp = new JPanel();
+		jp.setPreferredSize(new Dimension(100, 50));
+		jp.setBackground(Color.red);
+		jp.setOpaque(true);
+
+		JPanel jp2 = new JPanel();
+		jp2.setPreferredSize(new Dimension(100, 100));
+		jp2.setBackground(Color.green);
+		jp2.setOpaque(true);
+
+		JPanel jp3 = new JPanel();
+		jp3.setPreferredSize(new Dimension(100, 100));
+		jp3.setBackground(Color.blue);
+		jp3.setOpaque(true);
+
+		JPanel jp4 = new JPanel();
+		jp4.setPreferredSize(new Dimension(100, 100));
+		jp4.setBackground(Color.yellow);
+		jp4.setOpaque(true);
+
+		JPanel jp5 = new JPanel();
+		jp5.setPreferredSize(new Dimension(100, 100));
+		jp5.setBackground(Color.black);
+		jp4.setOpaque(true);
+
 		
+		
+
+		mainPanel.add(jp, BorderLayout.PAGE_START);
+		mainPanel.add(jp2, BorderLayout.PAGE_START);
+		mainPanel.add(jp3, BorderLayout.PAGE_START);
+		mainPanel.add(jp4, BorderLayout.PAGE_START);
+		mainPanel.add(jp5, BorderLayout.PAGE_START);
+		
+
+		return mainPanel;
+	}
+
+	public void close() {
+		// frame.dispatchEvent(new WindowEvent(frame,
+		// WindowEvent.WINDOW_CLOSING));
 	}
 }

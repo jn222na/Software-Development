59d0ec2b8a4ccba234a4357eaf253ffc1da9fa9f
jn222na <jn222na@student.lnu.se>
2015-11-09 14:02:05 +0100

    TEST 003 RED
    TestColorChoice

--- 04-dt007g-joni510/src/Tests/TestSwingApp.java
+++ 04-dt007g-joni510/src/Tests/TestSwingApp.java
@@ -6,8 +6,13 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.awt.event.WindowEvent;
+import java.awt.image.ColorModel;
+import java.util.ArrayList;
+
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.junit.*;
 import View.MainView;
 
@@ -31,61 +36,92 @@ public class TestSwingApp {
 //			mv.close();
 	}
 	
+//	@Test
+//	public void testSystemButtons(){
+//		//Minimise
+//		//Fullscreen / !Fullscreen
+//		//Exit
+//	}
+//	@Test
+//	public void testTitleBar(){
+//		//Move
+//		//Minimise
+//		//Close
+//	}
+//	@Test
+//	public void testComboBox(){
+//		MainView mv = new MainView();
+//		mv.drawWindow();
+//		JComboBox<String> box = mv.jcombo();
+//		try {
+//			//Freehand tool
+//			
+//			Robot bot = new Robot();
+//			bot.mouseMove(1260, 350);
+//			
+//			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//			bot.delay(1000);
+//			bot.mouseMove(1260, 400);
+//			bot.delay(1000);
+//			bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+//			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//			bot.delay(1000);
+//			assertTrue(box.getSelectedItem() == "Frihand");
+//		} catch (AWTException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			//Rectangular tool
+//			Robot bot = new Robot();
+//			bot.mouseMove(1260, 350);
+//			
+//			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//			bot.delay(1000);
+//			bot.mouseMove(1260, 390);
+//			bot.delay(1000);
+//			bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+//			bot.delay(1000);
+//			assertTrue(box.getSelectedItem() == "Rektangel");
+//		}catch (AWTException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
+	/**
+	 * Tests that the colors we are printing out is the same as we are picking.
+	 */
 	@Test
-	public void testSystemButtons(){
-		//Minimise
-		//Fullscreen / !Fullscreen
-		//Exit
-	}
-	@Test
-	public void testTitleBar(){
-		//Move
-		//Minimise
-		//Close
-	}
-	@Test
-	public void testComboBox(){
+	public void testColorChoice(){
+		
 		MainView mv = new MainView();
+		ArrayList<JPanel> list = mv.panels();
+		int botWidth = 750;
+		int botHeight = 350;
 		mv.drawWindow();
-		JComboBox<String> box = mv.jcombo();
+		
 		try {
-			//Freehand tool
-			
 			Robot bot = new Robot();
-			bot.mouseMove(1260, 350);
 			
-			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-			bot.delay(1000);
-			bot.mouseMove(1260, 400);
-			bot.delay(1000);
-			bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-			bot.delay(1000);
-			assertTrue(box.getSelectedItem() == "Frihand");
+			assertFalse(list.isEmpty());
+			for (int i = 0; i < list.size(); i++) {
+				bot.mouseMove(botWidth, botHeight);
+				bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+				bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);bot.delay(1000);
+				assertTrue(bot.getPixelColor(botWidth, botHeight).equals(list.get(i).getBackground()));
+				botWidth +=100;
+				
+			}bot.delay(2000);
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		try {
-			//Rectangular tool
-			Robot bot = new Robot();
-			bot.mouseMove(1260, 350);
-			
-			bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-			bot.delay(1000);
-			bot.mouseMove(1260, 390);
-			bot.delay(1000);
-			bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-			bot.delay(1000);
-			assertTrue(box.getSelectedItem() == "Rektangel");
-		}catch (AWTException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//Colorchoice update
+		
 	}
-	
-
 
 	
 
--- 04-dt007g-joni510/src/View/MainView.java
+++ 04-dt007g-joni510/src/View/MainView.java
@@ -4,19 +4,16 @@ import java.awt.*;
 
 import java.awt.Dimension;
 import java.awt.event.WindowEvent;
-
+import java.util.ArrayList;
 
 import javax.swing.*;
 
 public class MainView {
 
-	public MainView() {
 
-		
-	}
 	JFrame frame = new JFrame("Ritprogram");
 	JComboBox<String> box = new JComboBox<String>();
-
+	ArrayList<JPanel> list = new ArrayList<JPanel>();
 	public JFrame drawWindow() {
 
 		frame.setVisible(true);
@@ -24,14 +21,19 @@ public class MainView {
 		frame.setBounds(700, 250, 600, 500);
 		frame.setLayout(new BorderLayout());
 
+		JPanel jpBottom = buildBottom();
 		JPanel mainPanel = buildTopGrid();
 		frame.add(mainPanel, BorderLayout.PAGE_START);
+		frame.add(jpBottom, BorderLayout.PAGE_END);
 		return frame;
 	}
 	public JComboBox<String> jcombo(){
 		return this.box;
 	}
-
+	
+	public ArrayList<JPanel> panels(){
+		return list;
+	}
 	private JPanel buildTopGrid() {
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(new GridLayout(1, 6));
@@ -42,27 +44,30 @@ public class MainView {
 		jp.setPreferredSize(new Dimension(100, 50));
 		jp.setBackground(Color.red);
 		jp.setOpaque(true);
-
+		
 		JPanel jp2 = new JPanel();
 		jp2.setPreferredSize(new Dimension(100, 100));
 		jp2.setBackground(Color.green);
 		jp2.setOpaque(true);
-
+		
+		
 		JPanel jp3 = new JPanel();
 		jp3.setPreferredSize(new Dimension(100, 100));
 		jp3.setBackground(Color.blue);
 		jp3.setOpaque(true);
-
+		
+		
 		JPanel jp4 = new JPanel();
 		jp4.setPreferredSize(new Dimension(100, 100));
 		jp4.setBackground(Color.yellow);
 		jp4.setOpaque(true);
-
+		
+		
 		JPanel jp5 = new JPanel();
 		jp5.setPreferredSize(new Dimension(100, 100));
 		jp5.setBackground(Color.black);
-		jp4.setOpaque(true);
-
+		jp5.setOpaque(true);
+		
 		
 		box.setVisible(true);
 		box.addItem("Rektangel");
@@ -79,7 +84,30 @@ public class MainView {
 
 		return mainPanel;
 	}
+	private JPanel buildBottom() {
+		JPanel jpBottom = new JPanel();
+		jpBottom.setPreferredSize(new Dimension(frame.getWidth(), 30));
+		jpBottom.setBackground(Color.lightGray);
+		jpBottom.setLayout(new BorderLayout());
 
+		JPanel chosenColorPanel = new JPanel();
+		chosenColorPanel.setLayout(new BorderLayout());
+		chosenColorPanel.setBackground(Color.lightGray);
+
+		JPanel chosenColorPanel2 = new JPanel();
+		chosenColorPanel2.setLayout(new FlowLayout());
+		chosenColorPanel2.setBackground(Color.red);
+		chosenColorPanel2.setPreferredSize(new Dimension(50, 0));
+		jpBottom.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+		JLabel jlabelCoords = new JLabel("Koordinater : 250, 100");
+		JLabel chosenColor = new JLabel("Färgval : ");
+		jpBottom.add(jlabelCoords, BorderLayout.LINE_START);
+		chosenColorPanel.add(chosenColor, BorderLayout.CENTER);
+		chosenColorPanel.add(chosenColorPanel2, BorderLayout.LINE_END);
+		jpBottom.add(chosenColorPanel, BorderLayout.EAST);
+		return jpBottom;
+	}
 	public void close() {
 		// frame.dispatchEvent(new WindowEvent(frame,
 		// WindowEvent.WINDOW_CLOSING));
